e30e8061d72b0dd7e0a4cd3c9c3a7730826dcc28
Varnerlab <jdv27@cornell.edu>
2017-03-30 12:43:00 -0400

    Dynamic reaction

--- src/DataDictionary.jl
+++ src/DataDictionary.jl
@@ -35,27 +35,30 @@ function maximize_cellmass_data_dictionary(time_start,time_stop,time_step)
 	default_flux_bounds_array = data_dictionary["default_flux_bounds_array"]
 	default_flux_bounds_array[21,2] = 0.0
 
+	# ATP maintenance -
+	# default_flux_bounds_array[20,1:2] = 7.6
+
 	# setup exchange array -
 	species_bounds_array = data_dictionary["species_bounds_array"]
 	exchange_array = [
-		0.0	0.0	;	# 73 M_ac_b
+		0.0	10.0	;	# 73 M_ac_b
 		0.0	0.0	;	# 74 M_acald_b
 		0.0	0.0	;	# 75 M_akg_b
-		0.0	1.0	;	# 76 M_co2_b
+		0.0	100.0	;	# 76 M_co2_b
 		0.0	0.0	;	# 77 M_etoh_b
-		0.0	1.0	;	# 78 M_for_b
-		0.0	1.0	;	# 79 M_fru_b
-		0.0	1.0	;	# 80 M_fum_b
+		0.0	0.0	;	# 78 M_for_b
+		0.0	0.0	;	# 79 M_fru_b
+		0.0	0.0	;	# 80 M_fum_b
 		-1.0	0.0	;	# 81 M_glc_D_b
 		0.0	0.0	;	# 82 M_gln_L_b
 		0.0	0.0	;	# 83 M_glu_L_b
-		-1.0	1.0	;	# 84 M_h2o_b
-		-1.0	1.0	;	# 85 M_h_b
+		-10.0	10.0	;	# 84 M_h2o_b
+		-100.0	100.0	;	# 85 M_h_b
 		0.0	0.0	;	# 86 M_lac_D_b
 		0.0	0.0	;	# 87 M_mal_L_b
-		-1.0	1.0	;	# 88 M_nh4_b
-		-1.0	1.0	;	# 89 M_o2_b
-		-1.0	1.0	;	# 90 M_pi_b
+		-10.0	10.0	;	# 88 M_nh4_b
+		-15.0	0.0	;	# 89 M_o2_b
+		-10.0	10.0	;	# 90 M_pi_b
 		0.0	0.0	;	# 91 M_pyr_b
 		0.0	0.0	;	# 92 M_succ_b
 	]
@@ -97,7 +100,7 @@ function maximize_atp_data_dictionary(time_start,time_stop,time_step)
 		0.0	0.0	;	# 73 M_ac_b
 		0.0	0.0	;	# 74 M_acald_b
 		0.0	0.0	;	# 75 M_akg_b
-		0.0	1.0	;	# 76 M_co2_b
+		0.0	100.0	;	# 76 M_co2_b
 		0.0	0.0	;	# 77 M_etoh_b
 		0.0	1.0	;	# 78 M_for_b
 		0.0	1.0	;	# 79 M_fru_b
@@ -110,7 +113,7 @@ function maximize_atp_data_dictionary(time_start,time_stop,time_step)
 		0.0	0.0	;	# 86 M_lac_D_b
 		0.0	0.0	;	# 87 M_mal_L_b
 		-1.0	1.0	;	# 88 M_nh4_b
-		-1.0	1.0	;	# 89 M_o2_b
+		-100.0	0.0	;	# 89 M_o2_b
 		-1.0	1.0	;	# 90 M_pi_b
 		0.0	0.0	;	# 91 M_pyr_b
 		0.0	0.0	;	# 92 M_succ_b
--- src/Include.jl
+++ src/Include.jl
@@ -2,3 +2,6 @@ include("DataDictionary.jl")
 include("FluxDriver.jl")
 include("Utility.jl")
 using GLPK
+
+# my changes -
+using PyPlot
